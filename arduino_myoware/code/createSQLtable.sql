824cffb3bcaa1342f89bef0599d71ddee4cafde8
emilyrodgers726 <erodgers726@gmail.com>
2024-04-28 15:56:49 -0700

    having problems saving, trying again

--- arduino_myoware/code/createSQLtable.sql
+++ arduino_myoware/code/createSQLtable.sql
@@ -1,3 +1,7 @@
+/* 
+this code is no longer valid since optimizing code to remove startTime and endTime 
+*/
+
 CREATE TABLE events (
   event_id SERIAL PRIMARY KEY,
   start_time TIMESTAMP NOT NULL,
@@ -11,3 +15,16 @@ CREATE TABLE sensor_readings (
   sensor_value INTEGER NOT NULL,
   FOREIGN KEY (event_id) REFERENCES events(event_id)
 );
+
+/*
+
+Notes for later:::
+
+Transferring Data to PostgreSQL
+    Once data is captured:
+        1. Transfer to a Server: Use a script or manual process to upload your CSV files to a server or directly to your database system.
+        2. Import CSV Data: Use PostgreSQL's COPY command to import data from the CSV files into the respective tables. 
+
+COPY events(event_id, start_time, end_time) FROM '/path_to_csv/events.csv' DELIMITER ',' CSV HEADER;
+COPY sensor_readings(event_id, timestamp, sensor_value) FROM '/path_to_csv/sensor_readings.csv' DELIMITER ',' CSV HEADER;
+*/
